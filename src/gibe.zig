58418fc6252742149b8ac08dc4b792b7e7c5dd69
Dennis Krasnov <dennis@krasnov.dev>
2025-05-25 22:19:52 -0400

    add missing re-exports

--- src/gibe.zig
+++ src/gibe.zig
@@ -2,6 +2,10 @@ const model = @import("model.zig");
 const server = @import("server.zig");
 
 pub const Request = model.Request;
+pub const Method = model.Method;
+pub const QueryParameter = model.QueryParameter;
+pub const Header = model.Header;
 pub const Response = model.Response;
+pub const StatusCode = model.StatusCode;
 
 pub const Server = server.Server;
